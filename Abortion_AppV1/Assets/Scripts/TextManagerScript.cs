70bb526af3584847cde2decdccc1c701c96b599b
James Taylor <taylorjames9@gmail.com>
2014-06-03 19:00:05 -0500

    Before removing main camera

--- Abortion_AppV1/Assets/Scripts/TextManagerScript.cs
+++ Abortion_AppV1/Assets/Scripts/TextManagerScript.cs
@@ -23,11 +23,11 @@ public class TextManagerScript : MonoBehaviour {
 						textPlace = value;
 				
 						if(textPlace == 0)
-								my3DTextObject.fontSize = 57;
+				my3DTextObject.fontSize = 40;
 						else if(textPlace == 1 || textPlace == 4 || textPlace == 7)
-								my3DTextObject.fontSize = 45;
+				my3DTextObject.fontSize = 30;
 						else
-								my3DTextObject.fontSize = 32;
+				my3DTextObject.fontSize = 20;
 
 						my3DTextObject.text = textList[textPlace];
 						print ("TextPlace =" + TextPlace);
@@ -40,16 +40,18 @@ public class TextManagerScript : MonoBehaviour {
 
 		//UILabel my3DTextObject = GetComponent<UILabel>();
 
-				textList.Add ("One Abortion.");
+		//my3DTextObject.text = textList[textPlace];
+
+				textList.Add ("One Abortion");
 				textList.Add ("Chapter 1: The Test");
-				textList.Add ("Lorem ipsum dolor sit amet, \n posuere turpis, at nam, \n viverra morbi imperdiet condimentum \npraesent eget donec, viverra \nconsequat");
-				textList.Add ("Nonummy placerat ut sed ipsum \ndignissim. Dui imperdiet, \nat in nullam suspendisse, wisi \nconsequat convallis wisi aliquam, \nnec et elementum in feugiat vel \ntempor. Sodales enim ultricies donec.");
+				textList.Add ("Lorem ipsum dolor sit amet, posuere turpis, at nam,  viverra morbi imperdiet condimentum praesent eget donec, viverra consequat");
+				textList.Add ("Nonummy placerat ut sed ipsum dignissim. Dui imperdiet, at in nullam suspendisse, wisi consequat convallis wisi aliquam, nec et elementum in feugiat vel tempor. Sodales enim ultricies donec.");
 				textList.Add ("Chapter 2: The Judge");
-				textList.Add ("Id qui risus. Ac non et \neget id faucibus, dapibus \ntincidunt. Sed tempor faucibus \ncondimentum,");
-				textList.Add ("Mi lobortis facilisi mauris \nvulputate, orci ipsum vel mi massa, \nullamcorper nulla, in dui quisque \nquisque at mauris ac, nulla \npurus. Sed nec, magna lorem \nconsectetuer justo quis metus, neque eu \nsuspendisse. Suscipit elit massa \nnec, laoreet varius morbi velit \net lorem elit, cursus imperdiet \naliquet");
+				textList.Add ("Id qui risus. Ac non et eget id faucibus, dapibus tincidunt. Sed tempor faucibus condimentum,");
+				textList.Add ("Mi lobortis facilisi mauris vulputate, orci ipsum vel mi massa, ullamcorper nulla, in dui quisque quisque at mauris ac, nulla purus. Sed nec, magna lorem consectetuer justo quis metus, neque eu suspendisse. Suscipit elit massa nec, laoreet varius morbi velit et lorem elit, cursus imperdiet aliquet");
 				textList.Add ("Chapter 3: The Parents");
-				textList.Add ("Id qui risus. Ac non et eget \nid faucibus, dapibus tincidunt. \nSed tempor faucibus condimentum");
-				textList.Add ("Sed nec, magna lorem consectetuer \njusto quis metus, neque eu \nsuspendisse. Suscipit elit massa \nnec, cursus imperdiet aliquet");
+				textList.Add ("Id qui risus. Ac non et eget id faucibus, dapibus tincidunt. Sed tempor faucibus condimentum");
+				textList.Add ("Sed nec, magna lorem consectetuer justo quis metus, neque eu suspendisse. Suscipit elit massa nec, cursus imperdiet aliquet");
 	}
 	
 	// Update is called once per frame
